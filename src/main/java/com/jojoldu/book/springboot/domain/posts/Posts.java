02702f40caac97b66ff9801e0523283eb5e892ee
boki <lsb530@naver.com>
2021-03-22 23:13:03 +0900

    Post 수정/조회 api And test

--- src/main/java/com/jojoldu/book/springboot/domain/posts/Posts.java
+++ src/main/java/com/jojoldu/book/springboot/domain/posts/Posts.java
@@ -27,4 +27,9 @@ public class Posts {
     public Posts(String title, String content, String author) {
         this.title = title; this.content = content; this.author = author;
     }
+
+    public void update(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
 }
